f50eaa949a16c8ab6e63455c711a56afe8bbbbc9
argianardi <argianardi1435@gmail.com>
2022-09-29 21:49:42 +0700

    import sass

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+import Script from "next/script";
+import "../styles/globals.css";
+import "../styles/scss/bootstrap.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://unpkg.com/react/umd/react.production.min.js"
+        crossorigin
+      />
+      <Script
+        src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+        crossorigin
+      />
+      <Script
+        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossorigin
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
