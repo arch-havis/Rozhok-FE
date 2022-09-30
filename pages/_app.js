02e693098e7b7937c8e2d7d69c44dc3e36d743c2
herbayulet <sayaherbayu@gmail.com>
2022-09-30 10:48:29 +0700

    new update

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,14 @@ import "../styles/globals.css";
 import "../styles/scss/bootstrap.scss";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Script
-        src="https://unpkg.com/react/umd/react.production.min.js"
-        crossorigin
-      />
-      <Script
-        src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
-        crossorigin
-      />
-      <Script
-        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
-        crossorigin
-      />
-      <Component {...pageProps} />;
-    </>
-  );
+    return (
+        <>
+            <Script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin />
+            <Script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin />
+            <Script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin />
+            <Component {...pageProps} />;
+        </>
+    );
 }
 
 export default MyApp;
