472be899c9a7831c44b85b2124fd40a9180df870
herbayulet <sayaherbayu@gmail.com>
2022-09-30 20:21:44 +0700

    add folder assets to save image and make new folder for porter-pages

---
+++ pages/porter-pages/porter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./dashboard";
+
+const Porter = () => {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    );
+};
+
+export default Porter;
