0b06bbb449c2f6f65d109ec850e70e031925f868
Adithia Sandi Josua Simangunsong <106661816+rtrdlmao@users.noreply.github.com>
2022-09-30 16:15:31 +0700

    update icon and title

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,23 @@ import "../styles/globals.css";
 import "../styles/scss/bootstrap.scss";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Script src="https://unpkg.com/react/umd/react.production.min.js" crossorigin />
-            <Script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" crossorigin />
-            <Script src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js" crossorigin />
-            <Component {...pageProps} />;
-        </>
-    );
+  return (
+    <>
+      <Script
+        src="https://unpkg.com/react/umd/react.production.min.js"
+        crossorigin
+      />
+      <Script
+        src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"
+        crossorigin
+      />
+      <Script
+        src="https://unpkg.com/react-bootstrap@next/dist/react-bootstrap.min.js"
+        crossorigin
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
