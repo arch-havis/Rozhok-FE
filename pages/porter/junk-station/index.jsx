3edab538037df7356d7aac891fb84043c0762168
herbayulet <sayaherbayu@gmail.com>
2022-10-02 21:10:41 +0700

    slicing detail penjemputan rosok

--- pages/porter/junk-station/index.jsx
+++ pages/porter/junk-station/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Card, Row, Button, Form } from "react-bootstrap";
 import HeaderPorter from "../../../components/HeaderPorter";
 import axios from "axios";
+import Router from "next/router";
 
 const Index = () => {
     const [dataProvinsi, setDataProvinsi] = useState([]);
@@ -15,6 +16,20 @@ const Index = () => {
         { id: 1, nama: "Rosok CBA", provinsi: "Jawa Timur", kota: "Trenggalek", kecamatam: "Panggul", jalan: "Jl. Wiro II Dusun hahaha RT 02/19" },
         { id: 2, nama: "Rosok ABC", provinsi: "Jawa Timur", kota: "Trenggalek", kecamatam: "Munjungan", jalan: "Jl. Wiro II Dusun hahaha RT 02/19" },
     ];
+
+    const DetailJunkStation = (item) => {
+        Router.push({
+            pathname: `/porter/junk-station/${item.id}`,
+            query: {
+                id: item.id,
+                nama: item.nama,
+                provinsi: item.provinsi,
+                kota: item.kota,
+                kecamatan: item.kecamatan,
+                jalan: item.jalan,
+            },
+        });
+    };
 
     // untuk mendapatkan list provinsi
     const getDataProvinsi = async () => {
@@ -131,7 +146,12 @@ const Index = () => {
                                             <Card.Title className="text-alpukat">Jalan: {client.jalan}</Card.Title>
                                         </div>
                                         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-flex justify-content-end">
-                                            <Button variant="alpukat" className="hover-overlay hover-zoom text-white fs-5" onChange={(e) => e.target.value(e)}>
+                                            <Button
+                                                variant="alpukat"
+                                                className="hover-overlay hover-zoom text-white fs-5"
+                                                onChange={(e) => e.target.value(e)}
+                                                onClick={() => DetailJunkStation(client)}
+                                            >
                                                 lihat
                                             </Button>
                                         </div>
