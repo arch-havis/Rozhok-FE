bc396754419b53d7abeff59a34f088d40a1141ac
herbayulet <sayaherbayu@gmail.com>
2022-10-02 21:15:32 +0700

    slicing detail penjemputan rosok

--- pages/porter/junk-station/[detailJunkStation].jsx
+++ pages/porter/junk-station/[detailJunkStation].jsx
@@ -13,15 +13,15 @@ const Index = () => {
                 <h3 className="text-alpukat float-end mt-5 fw-bolder">Detail Junk Station</h3>
                 <br />
                 <br />
-                <Row className="" style={{ marginTop: "100px" }}>
-                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
+                <Row className="d-flex justify-content-center" style={{ marginTop: "100px" }}>
+                    <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 d-flex justify-content-center">
                         <CgProfile className="text-alpukat" style={{ fontSize: "282.5px" }} />
                     </div>
                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-grid justify-content-end">
-                        <h1 className="text-alpukat">Provinsi: {router.query.provinsi}</h1>
-                        <h1 className="text-alpukat">Kota/Kab: {router.query.kota}</h1>
-                        <h1 className="text-alpukat">Kecamatan: {router.query.kecamatan}</h1>
-                        <h1 className="text-alpukat">Jalan: {router.query.jalan}</h1>
+                        <h1 className="text-alpukat fs-sm-5">Provinsi: {router.query.provinsi}</h1>
+                        <h1 className="text-alpukat fs-sm-5">Kota/Kab: {router.query.kota}</h1>
+                        <h1 className="text-alpukat fs-sm-5">Kecamatan: {router.query.kecamatan}</h1>
+                        <h1 className="text-alpukat fs-sm-5">Jalan: {router.query.jalan}</h1>
                     </div>
                 </Row>
             </div>
