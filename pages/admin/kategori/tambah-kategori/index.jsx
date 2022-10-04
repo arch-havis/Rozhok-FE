32576b479c82df715b6758aff834632db6a1ced0
argianardi <argianardi1435@gmail.com>
2022-10-04 09:33:48 +0700

    fix slicing tambah kategori

---
+++ pages/admin/kategori/tambah-kategori/index.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import HeaderAdmin from "../../../../components/HeaderAdmin";
+import Footer from "../../../../components/Footer";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+
+const Index = () => {
+  return (
+    <div>
+      <HeaderAdmin />
+      <Container className="py-5">
+        <Row className="pb-5">
+          <Col
+            md={8}
+            className="p-5 bg-putih  border border-lime rounded-2 mx-auto"
+          >
+            <Form.Group>
+              <Form.Label>Nama Kategori</Form.Label>
+              <Form.Control type="text"></Form.Control>
+              <Form.Label className="mt-2">Harga Klien</Form.Label>
+              <Form.Control type="text"></Form.Control>
+              <Form.Label className="mt-2">Harga Mitra</Form.Label>
+              <Form.Control type="text"></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Deskripsi</Form.Label>
+              <Form.Control as="textarea" rows={3}></Form.Control>
+            </Form.Group>
+            <Row id="bawah" className="mt-5">
+              <Col className="text-end">
+                <Button variant="lime">Tambah</Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
