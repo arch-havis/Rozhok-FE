53fd7a1f122cb3801c21843621abcfd0b12e4df7
argianardi <argianardi1435@gmail.com>
2022-10-04 07:52:51 +0700

    add dahsboard admin page

---
+++ pages/admin/dashboard/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import HeaderAdmin from "../../../components/HeaderAdmin";
+import Footer from "../../../components/Footer";
+import { Row, Col } from "react-bootstrap";
+
+const Index = () => {
+  return (
+    <div>
+      <HeaderAdmin />
+      <Row className="text-alpukat mt-5 m-0 text-center">
+        <Col md={1}></Col>
+        <Col
+          id="kiri"
+          md={4}
+          className="p-2 border border-alpukat rounded-2 bg-tea"
+        >
+          <Col>
+            <h6>Total Junk Station</h6>
+          </Col>
+          <Col>
+            <h4>
+              <b>15</b>
+            </h4>
+          </Col>
+        </Col>
+        <Col md={2}></Col>
+        <Col
+          id="kanan"
+          md={4}
+          className="p-2 border border-alpukat rounded-2 bg-tea"
+        >
+          <Col>
+            <h6>Total Junk Client</h6>
+          </Col>
+          <Col>
+            <h4>
+              <b>15</b>
+            </h4>
+          </Col>
+        </Col>
+        <Col md={1}></Col>
+      </Row>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
