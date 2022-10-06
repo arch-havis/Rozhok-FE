a3b373e037809bcb58c1caf79ea1460c13e865f0
herbayulet <sayaherbayu@gmail.com>
2022-10-06 21:53:39 +0700

    get data list penjemputan rosok use API

--- pages/porter/penjemputan-rosok/index.jsx
+++ pages/porter/penjemputan-rosok/index.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Row, Button } from "react-bootstrap";
 import HeaderPorter from "../../../components/HeaderPorter";
 import Router from "next/router";
+import Cookies from "js-cookie";
+import axios from "axios";
+import Footer from "../../../components/Footer";
 
 const Index = () => {
     // const router = useRouter();
-    // data dummy
-    const customer = [
-        { id: 1, nama: "Rudi", provinsi: "Jawa Timur", kota: "Trenggalek", kecamatam: "Panggul", jalan: "Jl. Wiro II Dusun hahaha RT 02/19" },
-        { id: 2, nama: "Hyung", provinsi: "Jawa Timur", kota: "Trenggalek", kecamatam: "Munjungan", jalan: "Jl. Wiro II Dusun hahaha RT 02/19" },
-    ];
-    const DetailRosok = (item) => {
+
+    const [dataListPenjemputan, setDataListPenjemputan] = useState([]);
+
+    const getDataPenjemputan = async () => {
+        try {
+            const response = await axios.get("https://altagp3.online/pengambilan/porter", {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("token")}`,
+                },
+            });
+            setDataListPenjemputan(response.data.data);
+            console.log(response.data.data);
+        } catch (error) {
+            if (error === "400") {
+                alert("masih ada yang salah");
+            }
+        }
+    };
+
+    useEffect(() => {
+        getDataPenjemputan();
+    }, []);
+
+    const DetailPenjemputan = (item) => {
         Router.push({
-            pathname: `/porter/penjemputan-rosok/${item.id}`,
+            pathname: `/porter/penjemputan-rosok/${item}`,
             query: {
-                id: item.id,
-                nama: item.nama,
-                provinsi: item.provinsi,
-                kota: item.kota,
-                kecamatan: item.kecamatan,
-                jalan: item.jalan,
+                item: item,
             },
         });
     };
@@ -27,29 +43,29 @@ const Index = () => {
     return (
         <div>
             <HeaderPorter />
-            <div className="container">
+            <div className="container" style={{ marginBottom: "100px" }}>
                 <h3 className="d-flex float-end text-alpukat fw-bolder mt-5">List Penjemputan</h3>
                 <br />
                 <br />
                 <div className="" style={{ marginTop: "80px" }}>
-                    {customer.map((client, i) => {
+                    {dataListPenjemputan.map((customer) => {
                         return (
-                            <Card className="w-100 mb-5 border border-lime" key={i}>
+                            <Card className="w-100 mb-5 border border-lime" key={customer.id_penjualan}>
                                 <Card.Body className="bg-tea shadow-md">
-                                    <Card.Title className="mb-4 text-alpukat">Nama: {client.nama}</Card.Title>
-                                    <Card.Title className="mb-4 text-alpukat">Provinsi: {client.provinsi}</Card.Title>
-                                    <Card.Title className="mb-4 text-alpukat">Kota: {client.kota}</Card.Title>
-                                    <Card.Title className="mb-4 text-alpukat">Kecamatan: {client.kecamatam}</Card.Title>
+                                    <Card.Title className="mb-4 text-alpukat">Nama: {customer.client.name}</Card.Title>
+                                    <Card.Title className="mb-4 text-alpukat">Provinsi: {customer.client.provinsi}</Card.Title>
+                                    <Card.Title className="mb-4 text-alpukat">Kota: {customer.client.kota}</Card.Title>
+                                    <Card.Title className="mb-4 text-alpukat">Kecamatan: {customer.client.kecamatan}</Card.Title>
                                     <Row>
                                         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
-                                            <Card.Title className="text-alpukat">Jalan: {client.jalan}</Card.Title>
+                                            <Card.Title className="text-alpukat"></Card.Title>
                                         </div>
                                         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-flex justify-content-end">
                                             <Button
                                                 variant="alpukat"
                                                 className="hover-overlay hover-zoom text-white fs-5"
                                                 onChange={(e) => e.target.value(e)}
-                                                onClick={() => DetailRosok(client)}
+                                                onClick={() => DetailPenjemputan(customer.id_penjualan)}
                                             >
                                                 lihat
                                             </Button>
@@ -61,6 +77,7 @@ const Index = () => {
                     })}
                 </div>
             </div>
+            <Footer />
         </div>
     );
 };
