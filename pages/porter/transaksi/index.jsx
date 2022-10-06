dca84413dc0aecfd6654df9959521d8591374b31
herbayulet <sayaherbayu@gmail.com>
2022-10-06 21:54:08 +0700

    get data list transaksi porter use API

--- pages/porter/transaksi/index.jsx
+++ pages/porter/transaksi/index.jsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderPorter from "../../../components/HeaderPorter";
 import { Form, Button, Table } from "react-bootstrap";
 import { AiTwotoneEdit, AiTwotoneDelete } from "react-icons/ai";
 import Router from "next/router";
+import Cookies from "js-cookie";
+import axios from "axios";
+import Footer from "../../../components/Footer";
+
 const Index = () => {
-    // data dummy
-    const transaksi = [
-        {
-            id: 1,
-            tipeTransaksi: "Pembelian",
-            status: "Sudah bayar",
-            nama: "Rudi",
-            provinsi: "Jawa Timur",
-            kota: "Trenggalek",
-            kecamatan: "Panggul",
-            jalan: "Jl. Wiro II Dusun hahaha RT 02/19",
-        },
-        {
-            id: 2,
-            tipeTransaksi: "Pembelian",
-            status: "Belum bayar",
-            nama: "Hyung",
-            provinsi: "Jawa Timur",
-            kota: "Trenggalek",
-            kecamatan: "Munjungan",
-            jalan: "Jl. Wiro II Dusun hihihi RT 03/05",
-        },
-    ];
+    const [dataTransaksi, setDataTransaksi] = useState([]);
+
+    const getDataTransaksi = async () => {
+        try {
+            const response = await axios.get("https://altagp3.online/transaksi/porter", {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("token")}`,
+                },
+            });
+            setDataTransaksi(response.data.data);
+            console.log(dataTransaksi);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getDataTransaksi();
+    }, []);
 
     const DetailTransaksi = (item) => {
         Router.push({
@@ -46,7 +46,7 @@ const Index = () => {
     return (
         <div>
             <HeaderPorter />
-            <div className="container">
+            <div className="container" style={{ marginBottom: "100px" }}>
                 <h3 className="d-flex float-end text-alpukat fw-bolder mt-5">List Transaksi</h3>
                 <br />
                 <br />
@@ -59,18 +59,12 @@ const Index = () => {
                         </Form.Select>
                         <Form.Select aria-label="Default select example" className="mx-0 mx-sm-2 my-2 my-sm-0" onChange={(e) => handleKecamatan(e)}>
                             <option>Status</option>
-                            <option value="sudah bayar">Sudah Bayar</option>
-                            <option value="belum bayar">Belum Bayar</option>
+                            <option value="sudah_bayar">Sudah Bayar</option>
+                            <option value="belum_bayar">Belum Bayar</option>
                         </Form.Select>
                         <Button variant="alpukat" className="ms-2">
                             Filter
                         </Button>
-                    </div>
-                </div>
-                <div className="d-flex justify-content-end mt-4">
-                    <div className="d-flex flex-sm-row flex-column w-50">
-                        <input type="date" className="rounded-2 border-1 border-putih w-50" />
-                        <input type="date" className="rounded-2 border-1 border-putih w-50 my-1 my-sm-0 ms-auto" />
                     </div>
                 </div>
                 <Table className="mt-5 rounded rounded-3 overflow-hidden mx-auto">
@@ -83,11 +77,11 @@ const Index = () => {
                         </tr>
                     </thead>
                     <tbody className="bg-tea">
-                        {transaksi.map((data) => {
+                        {dataTransaksi.map((data) => {
                             return (
-                                <tr key={data.id}>
-                                    <td>{data.id}</td>
-                                    <td>{data.tipeTransaksi}</td>
+                                <tr key={data.id_transaksi}>
+                                    <td>{data.id_transaksi}</td>
+                                    <td>{data.tipe_transaksi}</td>
                                     <td>{data.status}</td>
                                     <td>
                                         <AiTwotoneEdit className="fs-4 text-lime user-select-auto" onChange={(e) => e.target.value(e)} onClick={() => DetailTransaksi(data)} />
@@ -99,6 +93,7 @@ const Index = () => {
                     </tbody>
                 </Table>
             </div>
+            <Footer />
         </div>
     );
 };
