e095a40e5d2053bcad9df093989a85d530818615
herbayulet <sayaherbayu@gmail.com>
2022-10-06 21:53:14 +0700

    get detail data penjemputan rosok use API

--- pages/porter/penjemputan-rosok/[penjemputanRosok].jsx
+++ pages/porter/penjemputan-rosok/[penjemputanRosok].jsx
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Card, Button } from "react-bootstrap";
 import HeaderPorter from "../../../components/HeaderPorter";
 import { CgProfile } from "react-icons/cg";
 import { useRouter } from "next/router";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const Index = () => {
     const router = useRouter();
+
+    const [dataDetailPenjemputan, setDataDetailPenjemputan] = useState([]);
+
+    const detailPenjemputan = async () => {
+        var axios = require("axios");
+
+        var config = {
+            method: "get",
+            url: `https://altagp3.online/pengambilan/${router.query.item}/porter`,
+            headers: {
+                Authorization: `Bearer ${Cookies.get("token")}`,
+            },
+        };
+
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data.data));
+                setDataDetailPenjemputan(response.data.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };
+
+    useEffect(() => {
+        detailPenjemputan();
+    }, []);
+
+    console.log("ini router", router.query.item);
+
     return (
         <div>
             <HeaderPorter />
@@ -15,14 +47,13 @@ const Index = () => {
                 <br />
                 <Row className="" style={{ marginTop: "100px" }}>
                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6">
-                        <h4 className="text-alpukat">Nama: {router.query.nama}</h4>
+                        <h4 className="text-alpukat">Nama: {dataDetailPenjemputan.client.name}</h4>
                         <CgProfile className="text-alpukat" style={{ fontSize: "92.5px" }} />
                     </div>
                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-grid justify-content-end">
-                        <h4 className="text-alpukat">Provinsi: {router.query.provinsi}</h4>
-                        <h4 className="text-alpukat">Kota/Kab: {router.query.kota}</h4>
-                        <h4 className="text-alpukat">Kecamatan: {router.query.kecamatan}</h4>
-                        <h4 className="text-alpukat">Jalan: {router.query.jalan}</h4>
+                        <h4 className="text-alpukat">Provinsi: {dataDetailPenjemputan.client.provinsi}</h4>
+                        <h4 className="text-alpukat">Kota/Kab: {dataDetailPenjemputan.client.kota}</h4>
+                        <h4 className="text-alpukat">Kecamatan: {dataDetailPenjemputan.client.kecamatan}</h4>
                     </div>
                 </Row>
                 <Card className="w-100 mt-5 shadow-sm">
@@ -32,8 +63,8 @@ const Index = () => {
                             <Card.Title className="mb-4 text-alpukat ms-4">Kategori</Card.Title>
                         </div>
                         <div className="d-flex">
-                            <Card.Title className="mb-4 text-alpukat">1</Card.Title>
-                            <Card.Title className="mb-4 text-alpukat ms-5">Besi</Card.Title>
+                            <Card.Title className="mb-4 text-alpukat">{dataDetailPenjemputan.barang_rosok.id}</Card.Title>
+                            <Card.Title className="mb-4 text-alpukat ms-5">{dataDetailPenjemputan.barang_rosok.kategori}</Card.Title>
                         </div>
                     </Card.Body>
                 </Card>
