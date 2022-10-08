0cc6e05a08cf75e26e9d0a55e23ad0ddddffd450
Adithia Sandi Josua Simangunsong <106661816+rtrdlmao@users.noreply.github.com>
2022-10-08 22:08:05 +0700

    update transaksi

--- pages/client/transaksi/index.jsx
+++ pages/client/transaksi/index.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderClient from "../../../components/HeaderClient";
 import Footer from "../../../components/Footer";
 import { Row, Col, Form, Button, Table } from "react-bootstrap";
+import axios from "axios";
+import { getCookie } from "cookies-next";
 
-const Index = () => {
+export const getServerSideProps = async (context) => {
+  const token = getCookie("token", context);
+  const response = await fetch("https://altagp3.online/transaksi/client", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = await response.json();
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
+
+const Index = (props) => {
+  console.log(props.data);
+  ////////////////////////TEST///////////////////////////
+  const token = getCookie("token");
+  const [data, setData] = useState(props.data);
+  const handleClick = (e) => {
+    if (data === undefined) {
+      getData(e);
+    } else {
+      setData();
+    }
+  };
+  const getData = async (e) => {
+    e.preventDefault();
+    await axios({
+      method: "get",
+      url: "https://altagp3.online/transaksi/client",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  ////////////////////// START ///////////////////////////////
+  const [tipe, setTipe] = useState();
+  const [status, setStatus] = useState();
+  console.log(tipe, status);
+
+  const handleFilter = async (e) => {
+    e.preventDefault();
+    await axios({
+      method: "get",
+      url: "https://altagp3.online/transaksi/client",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        type_transaction: tipe,
+        status: status,
+      },
+    })
+      .then((response) => {
+        console.log(response.data.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <div>
       <HeaderClient />
@@ -15,6 +88,12 @@ const Index = () => {
       <div className="pt-0 p-5 me-5 ms-5">
         <div className="border-bottom border-tea border-2 text-end"></div>
       </div>
+
+      {/* //////////////////////////////TEST//////////////////////////////// */}
+      {/* <Button onClick={(e) => handleClick(e)}>
+        {data === undefined ? <>Kosong</> : <>Ada</>}
+      </Button> */}
+
       <Row>
         <Col md={2}></Col>
         <Col md={8}>
@@ -25,37 +104,54 @@ const Index = () => {
                 <Form.Label>
                   <b>Tipe Transaksi</b>
                 </Form.Label>
-                <Form.Select aria-label="Default select example">
+                <Form.Select
+                  aria-label="Default select example"
+                  onChange={(e) => setTipe(e.target.value)}
+                >
                   <option disabled selected>
                     Tipe Transaksi
                   </option>
-                  <option value="1">Penjualan</option>
-                  <option value="2">Pembelian</option>
+                  <option value="penjualan">Penjualan</option>
+                  <option value="pembelian">Pembelian</option>
                 </Form.Select>
               </Form.Group>
-              <Button variant="tea w-100">Tanggal Mulai</Button>
             </Col>
             <Col>
               <Form.Group className="mb-2">
                 <Form.Label>
                   <b>Status</b>
                 </Form.Label>
-                <Form.Select aria-label="Default select example">
+                <Form.Select
+                  aria-label="Default select example"
+                  onChange={(e) => setStatus(e.target.value)}
+                >
                   <option disabled selected>
                     Status
                   </option>
-                  <option value="1">Delivery</option>
-                  <option value="2">Pending</option>
-                  <option value="3">Paid</option>
-                  <option value="4">Success</option>
-                  <option value="5">Cancelled</option>
-                  <option value="6">Picking Up</option>
+                  <option value="dikirim">Dikirim</option>
+                  <option value="belum_bayar">Belum Bayar</option>
+                  <option value="dibayar">Dibayar</option>
+                  <option value="dalam_perjalanan">Dalam Perjalanan</option>
+                  <option value="dibatalkan">Dibatalkan</option>
+                  <option value="diterima">Diterima</option>
                 </Form.Select>
               </Form.Group>
-              <Button variant="tea w-100">Tanggal Akhir</Button>
             </Col>
             <Col className="text-center">
-              <Button variant="lime w-50">Filter</Button>
+              <Button
+                variant="lime w-50 text-white"
+                onClick={(e) => handleFilter(e)}
+              >
+                Filter
+              </Button>
+              <div>
+                <Button
+                  variant="danger w-50"
+                  onClick={() => window.location.reload()}
+                >
+                  Reset
+                </Button>
+              </div>
             </Col>
           </Row>
         </Col>
@@ -76,28 +172,37 @@ const Index = () => {
               </tr>
             </thead>
             <tbody className="border-lime border">
-              <tr>
-                <td>1</td>
-                <td>TF-192740</td>
-                <td>Penjualan</td>
-                <td>Success</td>
-                <td>
-                  <a href="/client/transaksi/penjualan" className="text-alpukat text-decoration-none">
-                    Detail
-                  </a>
-                </td>
-              </tr>
-              <tr>
-                <td>1</td>
-                <td>TF-89993</td>
-                <td>Pembelian</td>
-                <td>Success</td>
-                <td>
-                  <a href="/client/transaksi/pembelian" className="text-alpukat text-decoration-none">
-                    Detail
-                  </a>
-                </td>
-              </tr>
+              {data === undefined ? (
+                <></>
+              ) : (
+                data.data.map((items, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{index + 1}</td>
+                      <td>{items.kode_transaksi}</td>
+                      <td>
+                        {items.tipe_transaksi.charAt(0).toUpperCase() +
+                          items.tipe_transaksi.slice(1)}
+                      </td>
+                      <td>
+                        {items.status.split("_").map((items) => {
+                          return (
+                            items.charAt(0).toUpperCase() + items.slice(1) + " "
+                          );
+                        })}
+                      </td>
+                      <td>
+                        <a
+                          href="/client/transaksi/penjualan"
+                          className="text-alpukat text-decoration-none"
+                        >
+                          Detail
+                        </a>
+                      </td>
+                    </tr>
+                  );
+                })
+              )}
             </tbody>
           </Table>
         </Col>
