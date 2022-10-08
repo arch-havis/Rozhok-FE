234c5afca6ec1fb9798989971112a875b0632ebf
herbayulet <sayaherbayu@gmail.com>
2022-10-08 23:57:19 +0700

    get data transaksi admin done

--- pages/admin/transaksi/index.jsx
+++ pages/admin/transaksi/index.jsx
@@ -1,100 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeaderAdmin from "../../../components/HeaderAdmin";
 import { Form, Button, Table } from "react-bootstrap";
 import { AiTwotoneEdit } from "react-icons/ai";
 import Router from "next/router";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const Index = () => {
-    // data dummy
-    const transaksi = [
-        {
-            id: 1,
-            nama: "Herbayu",
-            hp: "0833456789",
-            jalan: "Jl. Dedede RT 08/02",
-            provinsi: "Jawa Timur",
-            kota: "Trenggalek",
-            kecamatan: "Munjungan",
-            kodeTransaksi: "TF-194270",
-            biayaTransaksi: "Rp 30.000",
-            tanggalDibuat: "12/02/2022",
-            status: "belum bayar",
-            kurir: "JNT",
-        },
-        {
-            id: 2,
-            nama: "Rudi",
-            hp: "08334566612",
-            jalan: "Jl. Dududu RT 02/02",
-            provinsi: "Jakarta",
-            kota: "Jakarta Timur",
-            kecamatan: "Cililitan",
-            kodeTransaksi: "TF-194271",
-            biayaTransaksi: "Rp 30.000",
-            tanggalDibuat: "04/03/2022",
-            status: "sudah bayar",
-            kurir: "Sicepat",
-        },
-        {
-            id: 3,
-            nama: "Andrew",
-            hp: "0813456789",
-            jalan: "Jl. Dododo RT 08/02",
-            provinsi: "Jakarta",
-            kota: "Jakarta Selatan",
-            kecamatan: "Pasar Minggu",
-            kodeTransaksi: "TF-194272",
-            biayaTransaksi: "Rp 30.000",
-            tanggalDibuat: "01/11/2022",
-            status: "dikirim",
-            kurir: "Sicepat",
-        },
-        {
-            id: 4,
-            nama: "Bayu",
-            hp: "0813356999",
-            jalan: "Jl. Dididi RT 01/02",
-            provinsi: "Jawa Barat",
-            kota: "Depok",
-            kecamatan: "Cinere",
-            kodeTransaksi: "TF-194273",
-            biayaTransaksi: "Rp 30.000",
-            tanggalDibuat: "01/08/2022",
-            status: "dikirim",
-            kurir: "JNT",
-        },
-        {
-            id: 5,
-            nama: "Ways",
-            hp: "0821456789",
-            jalan: "Jl. Dadada RT 01/12",
-            provinsi: "Jogjakarta",
-            kota: "Sleman",
-            kecamatan: "Ngemplak",
-            kodeTransaksi: "TF-194274",
-            biayaTransaksi: "Rp 30.000",
-            tanggalDibuat: "03/09/2022",
-            status: "dikirim",
-            kurir: "JNE",
-        },
-    ];
+    const [dataTransaksi, setDataTransaksi] = useState([]);
+
+    useEffect(() => {
+        getDataTransaksi();
+    }, []);
+
+    const getDataTransaksi = async () => {
+        try {
+            const response = await axios.get("https://altagp3.online/transaksi/admin", {
+                headers: {
+                    Authorization: `Bearer ${Cookies.get("token")}`,
+                },
+            });
+            setDataTransaksi(response.data.data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     // Push params use Router
     const DetailTransaksi = (item) => {
         Router.push({
-            pathname: `/admin/transaksi/${item.id}`,
+            pathname: `/admin/transaksi/${item}`,
             query: {
-                id: item.id,
-                nama: item.nama,
-                provinsi: item.provinsi,
-                kota: item.kota,
-                kecamatan: item.kecamatan,
-                jalan: item.jalan,
-                kodeTransaksi: item.kodeTransaksi,
-                biayaTransaksi: item.biayaTransaksi,
-                hp: item.hp,
-                kurir: item.kurir,
-                status: item.status,
+                item: item,
             },
         });
     };
@@ -126,22 +63,26 @@ const Index = () => {
                             <th>No</th>
                             <th>Kode Transaksi</th>
                             <th>Biaya Transaksi</th>
-                            <th>Tanggal dibuat</th>
+                            {/* <th>Tanggal dibuat</th> */}
                             <th>Status</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
                     <tbody className="bg-tea">
-                        {transaksi.map((data) => {
+                        {dataTransaksi.map((data, i) => {
+                            const total = new Intl.NumberFormat("id-ID", {
+                                style: "currency",
+                                currency: "IDR",
+                                currencyDisplay: "symbol",
+                            }).format(data.grand_total);
                             return (
-                                <tr key={data.id}>
-                                    <td>{data.id}</td>
-                                    <td>{data.kodeTransaksi}</td>
-                                    <td>{data.biayaTransaksi}</td>
-                                    <td>{data.tanggalDibuat}</td>
+                                <tr key={data.id_transaksi}>
+                                    <td>{i + 1}</td>
+                                    <td>{data.kode_transaksi}</td>
+                                    <td>{total}</td>
                                     <td>{data.status}</td>
                                     <td>
-                                        <AiTwotoneEdit className="fs-4 text-lime user-select-auto" onChange={(e) => e.target.value(e)} onClick={() => DetailTransaksi(data)} />
+                                        <AiTwotoneEdit className="fs-4 text-lime user-select-auto" onClick={() => DetailTransaksi(data.id_transaksi)} />
                                     </td>
                                 </tr>
                             );
