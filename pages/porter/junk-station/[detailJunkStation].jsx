631af419d7e60c4d61ef065cf346942cbd272478
herbayulet <sayaherbayu@gmail.com>
2022-10-09 15:17:08 +0700

    add code

--- pages/porter/junk-station/[detailJunkStation].jsx
+++ pages/porter/junk-station/[detailJunkStation].jsx
@@ -5,6 +5,7 @@ import HeaderPorter from "../../../components/HeaderPorter";
 import { useRouter } from "next/router";
 import axios from "axios";
 import Cookies from "js-cookie";
+import Footer from "../../../components/Footer";
 
 const Index = () => {
     const router = useRouter();
@@ -22,7 +23,7 @@ const Index = () => {
                 },
             });
             setDataJunkStation(response.data.data);
-            console.log("ini response.data.data", response.data.data);
+            console.log("ini response.data.data", JSON.stringify(response.data.data));
         } catch (error) {
             console.log(error);
         }
@@ -38,16 +39,20 @@ const Index = () => {
                 <Row className="d-flex justify-content-center" style={{ marginTop: "100px" }}>
                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 d-flex justify-content-center">
                         {/* <CgProfile className="text-alpukat" style={{ fontSize: "282.5px" }} /> */}
-                        <img src={dataJunkStation?.image_url} alt="gambar" style={{ width: "150px", height: "150px" }} />
+                        <img src={dataJunkStation?.image_url} className="rounded-circle shadow-lg" alt="gambar" style={{ width: "350px", height: "350px" }} />
                     </div>
                     <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6 d-grid justify-content-end">
-                        <h1 className="text-alpukat fs-sm-5">Provinsi: {dataJunkStation?.provinsi}</h1>
-                        <h1 className="text-alpukat fs-sm-5">Kota/Kab: {dataJunkStation?.kota}</h1>
-                        <h1 className="text-alpukat fs-sm-5">Kecamatan: {dataJunkStation?.kecamatan}</h1>
-                        <h1 className="text-alpukat fs-sm-5">Status: {dataJunkStation?.status_kemitraan}</h1>
+                        <h2 className="text-alpukat fs-sm-5">Nama Junk-Station: {dataJunkStation?.junk_station_name}</h2>
+                        <h2 className="text-alpukat fs-sm-5">Provinsi: {dataJunkStation?.provinsi}</h2>
+                        <h2 className="text-alpukat fs-sm-5">Kota/Kab: {dataJunkStation?.kota}</h2>
+                        <h2 className="text-alpukat fs-sm-5">Kecamatan: {dataJunkStation?.kecamatan}</h2>
+                        <h2 className="text-alpukat fs-sm-5">Jalan: {dataJunkStation?.jalan}</h2>
+                        <h2 className="text-alpukat fs-sm-5">Telepon: {dataJunkStation?.telp}</h2>
+                        <h2 className="text-alpukat fs-sm-5">Status: {dataJunkStation?.status_kemitraan}</h2>
                     </div>
                 </Row>
             </div>
+            <Footer />
         </div>
     );
 };
