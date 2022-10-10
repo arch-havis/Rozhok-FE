036a0cc425abf36dfeb38059028c435744dffab8
Adithia Sandi Josua Simangunsong <106661816+rtrdlmao@users.noreply.github.com>
2022-10-10 18:54:17 +0700

    fixing

--- pages/client/toko/index.jsx
+++ pages/client/toko/index.jsx
@@ -5,6 +5,7 @@ import Footer from "../../../components/Footer";
 import HeaderClient from "../../../components/HeaderClient";
 import axios from "axios";
 import { getCookie } from "cookies-next";
+import Link from "next/link";
 
 export const getServerSideProps = async () => {
   const getProduct = await axios.get("https://altagp3.online/products");
@@ -63,7 +64,7 @@ const Index = (props) => {
       </div>
       <div className="pt-0 p-5 me-5 ms-5">
         <div className="border-bottom border-gray border-2 text-end">
-          <a href="/client/toko/cart" className="text-alpukat">
+          <Link href="/client/toko/cart">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -74,7 +75,7 @@ const Index = (props) => {
             >
               <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
             </svg>
-          </a>
+          </Link>
         </div>
       </div>
       <div id="product" className="mb-5">
